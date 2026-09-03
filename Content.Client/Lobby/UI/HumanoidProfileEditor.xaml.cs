d35f422d339d28764cbd2890d9c77c321d3614a3
LightLava <pasha.notch@gmail.com>
2026-09-04 00:35:01 +0700

    tweak: фикс поломанных структур лаваленда у корвухов

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -1004,6 +1004,7 @@ namespace Content.Client.Lobby.UI
                 settings.Add(setting);
             }
 
+            settings.Sort(SettingUIComparer.Instance);
             departments.Sort(DepartmentUIComparer.Instance);
 
             var items = new[]
